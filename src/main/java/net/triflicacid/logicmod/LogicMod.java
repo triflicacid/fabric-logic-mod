78d7b123f0cae5d0bda01a429ae621b74fa9a7cd
triflicacid <rubensaunders@hotmail.co.uk>
2023-04-02 19:03:59 +0100

    Initial

---
+++ src/main/java/net/triflicacid/logicmod/LogicMod.java
@@ -0,0 +1,16 @@
+package net.triflicacid.logicmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogicMod implements ModInitializer {
+	public static final String MOD_ID = "logicmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("logic-mod");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
