0e10009a677da31bbe9e6519307f3e09570840e4
triflicacid <rubensaunders@hotmail.co.uk>
2023-04-03 03:25:58 +0100

    Added missing loot tables for logic gates

--- src/main/java/net/triflicacid/logicmod/block/ModBlocks.java
+++ src/main/java/net/triflicacid/logicmod/block/ModBlocks.java
@@ -2,16 +2,12 @@ package net.triflicacid.logicmod.block;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.triflicacid.logicmod.LogicMod;
 import net.triflicacid.logicmod.block.custom.*;
