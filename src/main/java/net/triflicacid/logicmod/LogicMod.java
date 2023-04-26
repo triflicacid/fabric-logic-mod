86f807ed7e24923403b961fab428a08b5993014e
iceCream-Taco <66057937+iceCream-Taco@users.noreply.github.com>
2023-04-26 14:11:12 +0100

    Fixed wacky merge

--- src/main/java/net/triflicacid/logicmod/LogicMod.java
+++ src/main/java/net/triflicacid/logicmod/LogicMod.java
@@ -6,7 +6,6 @@ import net.triflicacid.logicmod.block.ModBlocks;
 import net.triflicacid.logicmod.blockentity.ModBlockEntity;
 import net.triflicacid.logicmod.item.ModItemGroup;
 import net.triflicacid.logicmod.item.ModItems;
-import net.triflicacid.logicmod.screen.ModScreenHandlers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,6 +44,5 @@ public class LogicMod implements ModInitializer {
 		ModBlockEntity.registerBlockEntities();
 		ModItems.registerItems();
 		ModBlocks.registerBlocks();
-		ModScreenHandlers.register();
 	}
 }
